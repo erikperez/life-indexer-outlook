56f167fa65c63dcca4ef208b4320731c38eeacb9
Erik Perez <erik.perez@comperiosearch.com>
2015-05-01 19:37:04 +0200

    Now writes crawl data to file. Added support for incremental crawling. In need of a switch argument [full|incremental]

--- configuration.js
+++ configuration.js
@@ -1,6 +1,8 @@
 exports.username = "YOUR USERNAME HERE"; //user@example.com
 exports.password = "YOUR PASSWORD HERE"; //123456
 
+//Advice: grab a json from via your browser and serve a simple http with that file.
+//You don't wanna spam the Azure API..
 exports.url = 'http://localhost:8089/Mail365.json';//"https://outlook.office365.com/ews/odata/Me/Folders/Inbox/Messages";
 exports.identifier = 'Id';
 exports.crawlfilename = 'O365.json';
--- indexer-basic.js
+++ indexer-basic.js
@@ -1,40 +1,98 @@
 var request = require('request'),
-    credentials = require('./credentials.js');
+    configuration = require('./configuration.js'),
+    fs = require('fs'),
+    _ = require('lodash');
 
-var username = credentials.username,
-    password = credentials.password;
+//Configuration values
+var username = configuration.username,
+    password = configuration.password,
+    crawlfilename = configuration.crawlfilename,
+    url = configuration.url;
 
-var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+//Checking if there is existing crawl data.
+var crawledFiles;
+var existingDataSwitch = false;
 
-var options = {
-  url: 'https://outlook.office365.com/ews/odata/Me/Folders/Inbox/Messages',
-  headers: {
-    authorization: auth
-  }
-};
-
-var emailSummary = [];
-
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var data = JSON.parse(body);
-    for (var i = 0; i < data.value.length; i++) {
-      var o = {
-        Subject: data.value[i].Subject,
-        BodyPreview: data.value[i].BodyPreview,
-        CreatedOn: new Date(data.value[i].DateTimeCreated),
-        From: data.value[i].From,
-        ToRecipients: data.value[i].ToRecipients,
-        CcRecipients: data.value[i].CcRecipients,
-        Link: data.value[i].WebLink,
-        ConversationId: data.value[i].ConversationId
-      };
-
-      emailSummary.push(o);
+fs.readFile(crawlfilename, function(err, data) {
+    if (data && data.length > 0) {
+        crawledFiles = JSON.parse(data);
+        if (crawledFiles && crawledFiles.data.length > 0) {
+            existingDataSwitch = true;
+            console.log("Existing data:" + existingDataSwitch);
+        }
     }
+});
 
-    console.log(emailSummary);
-  }
+
+//Building the request to O365
+var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+var options = {
+    url: url,
+    headers: {
+        authorization: auth
+    }
 };
 
-request(options, callback);
+request(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body).value;
+        var newEmails = false;
+
+        if (existingDataSwitch) {
+            var emails = _.filter(data, function(itemX) {
+                var subFilter = _.filter(crawledFiles.data, function(itemY) {
+                    return itemX[configuration.identifier] === itemY[configuration.identifier];
+                });
+                if (subFilter && subFilter.length === 0)
+                    return true;
+            });
+
+            data = emails;
+
+            if (emails && emails.length > 0) {
+                newEmails = true;
+                console.log("New emails: " + emails.length);
+            }
+        }
+
+        var crawlData = existingDataSwitch ? crawledFiles : {
+            data: [],
+            meta: {}
+        };
+
+        if (!newEmails && existingDataSwitch) {
+            console.log("No new emails");
+            return updateCrawlData(crawlData);
+        }
+
+        for (var i = 0; i < data.length; i++) {
+            var o = {
+                Id: data[i].Id,
+                Subject: data[i].Subject,
+                BodyPreview: data[i].BodyPreview,
+                CreatedOn: new Date(data[i].DateTimeCreated),
+                From: data[i].From,
+                ToRecipients: data[i].ToRecipients,
+                CcRecipients: data[i].CcRecipients,
+                Url: data[i].WebLink,
+                ConversationId: data[i].ConversationId
+            };
+
+            crawlData.data.push(o);
+        }
+
+
+        return updateCrawlData(crawlData);
+
+    }
+});
+
+
+updateCrawlData = function(crawlData) {
+    crawlData.meta.crawldate = new Date();
+
+    fs.writeFile(crawlfilename, JSON.stringify(crawlData), function(err) {
+        if (err) throw err;
+        console.log("Crawl saved.");
+    });
+}
