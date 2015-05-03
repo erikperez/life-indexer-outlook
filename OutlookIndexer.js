74d888429b6de1d77f2ec7ea68a0f2be80b966f8
Erik Perez <erik.perez@comperiosearch.com>
2015-05-03 14:53:40 +0200

    Refactored to callback layout

--- OutlookIndexer.js
+++ OutlookIndexer.js
@@ -20,17 +20,81 @@ var OutlookIndexer = function OutlookIndexer(options) {
 
 	var self = this;
 
-	this._run = function _run(filterCallback, mappingCallback, storageCallback, indexCallback) {
+	this._filterCallback = function filterCallback(existingDataObject, data) {
+		console.log("Filtering " + data.length + " items...");
+		if (existingDataObject.crawlDataExists) {
+			data = _.filter(data, function(itemX) {
+				if (itemX !== null) {
+					var subFilter = _.filter(existingDataObject.crawlData.data, function(itemY) {
+						if (itemY !== null && itemX !== null) {
+							var identifier = self.options.identifier;
+							return itemX[self.options.identifier] === itemY[self.options.identifier];
+						}
+					})
+					console.log(subFilter.length)
+					if (subFilter && subFilter.length === 0)
+						return true;
+				}
+			});
+		}
+		console.log("Filtering done, result=" + data.length + " id[0]: "+data[0].Id);
+		return data;
+	}
 
-
-		//Building the request to O365
-		var auth = 'Basic ' + new Buffer(self.options.username + ':' + self.options.password).toString('base64');
-		var reqOptions = {
-			url: self.options.url,
-			headers: {
-				authorization: auth
+	this._mappingCallback = function mappingCallback(data) {
+		console.log("Mapping " + data.length + " items...");
+		var mappedData = [];
+		_.forEach(data, function(item) {
+			if (item !== undefined) {
+				var o = {
+					Id: item.Id,
+					Subject: item.Subject,
+					BodyPreview: item.BodyPreview,
+					CreatedOn: new Date(item.DateTimeCreated),
+					From: item.From,
+					ToRecipients: item.ToRecipients,
+					CcRecipients: item.CcRecipients,
+					Url: item.WebLink,
+					ConversationId: item.ConversationId
+				};
+				mappedData.push(o);
 			}
-		};
+		});
+		return mappedData;
+	}
+
+
+	this._storageCallback = function storageCallback(existingDataObject, mappedData) {
+		console.log("Storing " + mappedData.length + ' items...');
+		if (existingDataObject && existingDataObject.crawlDataExists) {
+
+			existingDataObject.crawlData.data.push(mappedData);
+			existingDataObject.crawlData.meta.crawldate = new Date(); //Set crawl date
+
+			fs.writeFile("crawlfilename.json", JSON.stringify(existingDataObject.crawlData), function(err) {
+				if (err) throw err;
+				console.log("Crawl saved.");
+			});
+		} else {
+			existingDataObject = {
+				crawlData: {
+					meta: {
+						crawldate: new Date()
+					},
+					data: mappedData
+				}
+			}
+
+			fs.writeFile("crawlfilename.json", JSON.stringify(existingDataObject.crawlData), function(err) {
+				if (err) throw err;
+				console.log("Crawl saved.");
+			});
+		}
+		console.log("Storage done");
+		return existingDataObject;
+	}
+
+	this._existingCrawlDataCallback = function existingCrawlDataCallback(dataCallback, filterCallback, mappingCallback, storageCallback, indexCallback) {
 
 		//Checking if there is existing crawl data somewhere.
 		fs.readFile("crawlfilename.json", function(err, data) {
@@ -50,108 +114,57 @@ var OutlookIndexer = function OutlookIndexer(options) {
 				crawlData: crawledFiles
 			}
 
-			request(reqOptions, function(error, response, body) {
-
-				if (!error && response.statusCode == 200) {
-					var fetchedData = JSON.parse(body).value;
-					var filteredData, mappedData, processedDataObject;
-
-					if (filterCallback && typeof filterCallback === 'function') {
-						filteredData = filterCallback(crawlDataObject, fetchedData);
-					}
-
-					if (mappingCallback && typeof mappingCallback === 'function') {
-						mappedData = mappingCallback(filteredData);
-					}
-
-					if (storageCallback && typeof storageCallback === 'function') {
-						processedDataObject = storageCallback(crawlDataObject, mappedData);
-					}
-
-					if (indexCallback && typeof indexCallback === 'function') {
-						console.log('Indexing ' + filteredData.length + ' items...');
-						var indexData = indexCallback(mappedData);
-						console.log("Indexing done");
-					}
-				}
-			});
-
+			if (dataCallback && typeof dataCallback == 'function') {
+				dataCallback(crawlDataObject, filterCallback, mappingCallback, storageCallback, indexCallback);
+			}
 		});
+	}
+
+	this._dataCallback = function dataCallback(crawlDataObject, filterCallback, mappingCallback, storageCallback, indexCallback) {
+
+		//Building the request to O365
+		var auth = 'Basic ' + new Buffer(self.options.username + ':' + self.options.password).toString('base64');
+		var reqOptions = {
+			url: self.options.url,
+			headers: {
+				authorization: auth
+			}
+		};
+
+		request(reqOptions, function(error, response, body) {
+
+			if (!error && response.statusCode == 200) {
+				var fetchedData = JSON.parse(body).value;
+				var filteredData, mappedData, processedDataObject;
+
+				if (filterCallback && typeof filterCallback === 'function') {
+					filteredData = filterCallback(crawlDataObject, fetchedData);
+					if (filteredData && mappingCallback && typeof mappingCallback === 'function') {
+						mappedData = mappingCallback(filteredData);
+						if (mappedData && storageCallback && typeof storageCallback === 'function') {
+							processedDataObject = storageCallback(crawlDataObject, mappedData);
+							if (indexCallback && typeof indexCallback === 'function') {
+								console.log('Indexing ' + filteredData.length + ' items...');
+								var indexData = indexCallback(mappedData);
+								console.log("Indexing done");
+							}
+						}
+					}
+
+				}
+			}
+		});
+	}
+
+	this._run = function _run(existingCrawlDataCallback, dataCallback, filterCallback, mappingCallback, storageCallback, indexCallback) {
+		existingCrawlDataCallback(dataCallback, filterCallback, mappingCallback, storageCallback, indexCallback);
 	};
 }
 
 OutlookIndexer.prototype = {
 	fetch: function fetch(storageCallback, indexCallback) {
 		var self = this;
-		self._run(function filterCallback(existingDataObject, data) {
-				console.log("Filtering " + data.length + " items...");
-				if (existingDataObject.crawlDataExists) {
-					return _.filter(data, function(itemX) {
-						if (itemX !== null) {
-							var subFilter = _.filter(existingDataObject.crawlData.data, function(itemY) {
-								if (itemY !== null && itemX !== null) {
-									var identifier = self.options.identifier;
-									return itemX[self.options.identifier] === itemY[self.options.identifier];
-								}
-							})
-							console.log(subFilter.length)
-							if (subFilter && subFilter.length === 0)
-								return true;
-						}
-					});
-				}
-				console.log("Filtering done, result=" + data.length);
-				return data;
-			}, function mappingCallback(data) {
-				console.log("Mapping " + data.length + " items...");
-				var mappedData = [];
-				_.forEach(data, function(item) {
-					if (item !== undefined) {
-						var o = {
-							Id: item.Id,
-							Subject: item.Subject,
-							BodyPreview: item.BodyPreview,
-							CreatedOn: new Date(item.DateTimeCreated),
-							From: item.From,
-							ToRecipients: item.ToRecipients,
-							CcRecipients: item.CcRecipients,
-							Url: item.WebLink,
-							ConversationId: item.ConversationId
-						};
-						mappedData.push(o);
-					}
-				});
-				return mappedData;
-			}, function storageCallback(existingDataObject, mappedData) {
-				console.log("Storing " + mappedData.length + ' items...');
-				if (existingDataObject && existingDataObject.crawlDataExists) {
-
-					existingDataObject.crawlData.data.push(mappedData);
-					existingDataObject.crawlData.meta.crawldate = new Date(); //Set crawl date
-
-					fs.writeFile("crawlfilename.json", JSON.stringify(existingDataObject.crawlData), function(err) {
-						if (err) throw err;
-						console.log("Crawl saved.");
-					});
-				} else {
-					existingDataObject = {
-						crawlData: {
-							meta: {
-								crawldate: new Date()
-							},
-							data: mappedData
-						}
-					}
-
-					fs.writeFile("crawlfilename.json", JSON.stringify(existingDataObject.crawlData), function(err) {
-						if (err) throw err;
-						console.log("Crawl saved.");
-					});
-				}
-				console.log("Storage done");
-				return existingDataObject;
-			},
-			indexCallback);
+		self._run(self._existingCrawlDataCallback, self._dataCallback, self._filterCallback, self._mappingCallback, self._storageCallback, indexCallback);
 
 	}
 };
